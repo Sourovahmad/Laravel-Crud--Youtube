32c2313f9e5cefad211a6afafe6de2a709168afd
sourovahmad <mdsourovahmad19@gmail.com>
2022-01-28 19:42:24 +0600

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,6 +34,14 @@
                         @endif
                     @endauth
                 </div>
+
+
+
+
+
+
+
+                
             @endif
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
