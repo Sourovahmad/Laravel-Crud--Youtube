96838819673eca1bbca58d25d57ae67a970c2211
sourovahmad <mdsourovahmad19@gmail.com>
2022-01-31 14:51:23 +0600

    crud complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\StudentController;
+use App\Models\student;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [StudentController::class, 'index']);
+
+Route::get('create', [StudentController::class, 'create'])->name('create');
+Route::post('store', [StudentController::class, 'store'])->name('store');
+
+Route::get('edit/{student_id}', [StudentController::class, 'update'])->name('edit');
+
+Route::post('edit-store', [StudentController::class, 'editStore'])->name('editStore');
+
+Route::delete('delete', [StudentController::class, 'destroy'])->name('delete');
+
